42e834d71c26c6b172b9f0a2962e32d003ac75f6
amichal2 <andrzej.michalski.3@gmail.com>
2017-10-22 20:20:06 +0100

    Initial routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 import '../assets/css/styles.css';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent { 
+
+    title = 'Shop';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './not-found.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { OrderComponent } from './order/order.component';
+
+const APP_ROUTES: Routes = [
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'order', component: OrderComponent },
+  { path: '',   redirectTo: 'inventory', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      APP_ROUTES,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    InventoryComponent,
+    OrderComponent
   ],
   bootstrap: [ AppComponent ]
 })
